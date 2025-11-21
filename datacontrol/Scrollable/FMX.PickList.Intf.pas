dfb03c0f6a7e62bf6d11ddeee2c79a22e1d82f55
Kees Vermeulen <kees.vermeulen@a-dato.net>
2025-11-21 11:38:42 +0100

    Commit

---
+++ datacontrol/Scrollable/FMX.PickList.Intf.pas
@@ -0,0 +1,38 @@
+﻿unit FMX.PickList.Intf;
+
+interface
+
+uses
+  System_,
+  System.Collections;
+
+type
+  IPickListSupport = interface
+    ['{F670376D-2A80-4963-B53A-3EF994D0263C}']
+    function  get_PickList: IList;
+    procedure set_PickList(const Value: IList);
+
+    property PickList: IList read get_PickList write set_PickList;
+  end;
+
+  TDataItemWithText = record
+  public
+    Data: CObject;
+    Text: CString;
+
+    constructor Create(const AData: CObject; const AText: CString);
+  end;
+
+implementation
+
+{ TDataItemWithText }
+
+constructor TDataItemWithText.Create(const AData: CObject; const AText: CString);
+begin
+  Data := AData;
+  Text := AText;
+end;
+
+end.
+
+
