c173ac3c0f1749a61cd75a7e42cbc84e0ebf824a
Kees Vermeulen <kees.vermeulen@a-dato.net>
2025-12-17 17:01:37 +0100

    Commit

--- datacontrol/Scrollable/FMX.ScrollControl.WithRows.Intf.pas
+++ datacontrol/Scrollable/FMX.ScrollControl.WithRows.Intf.pas
@@ -117,7 +117,7 @@ type
     procedure UpdateLastSelection(const DataIndex, ViewListIndex: Integer; const DataItem: CObject);
 
     procedure UpdateSingleSelection(const DataIndex, ViewListIndex: Integer; const DataItem: CObject; KeepCurrentSelection: Boolean);
-    procedure AddToSelection(const DataIndex, ViewListIndex: Integer; const DataItem: CObject);
+    procedure AddToSelection(const DataIndex, ViewListIndex: Integer; const DataItem: CObject; ExpandCurrentSelection: Boolean);
     procedure Deselect(const DataIndex: Integer);
     procedure SelectedRowClicked(const DataIndex: Integer);
 
